722a2431bb023e9c6e88e4fd6d1bbadfe87b037a
zuiwuchang <zuiwuchang@gmail.com>
2025-08-09 14:43:20 +0800

    aes ecb

--- src/ejs/js/crypto.ts
+++ src/ejs/js/crypto.ts
@@ -2,34 +2,132 @@ declare namespace deps {
     class Pointer {
         readonly __id = "pointer"
     }
-    class Desc {
-        readonly __id = "desc"
-    }
+
     const AES: number
+    class ECB {
+        readonly __id = "desc"
+        readonly p: Pointer
+        readonly blocksize: number
+    }
 
-    function ecb_enc_block(cipher: number, key: string | Uint8Array, plaintext: string | Uint8Array): Uint8Array
-    function ecb_enc_block(cipher: number, key: string | Uint8Array, plaintext: string | Uint8Array, ciphertext: Uint8Array): number
-    function ecb_dec_block(cipher: number, key: string | Uint8Array, plaintext: Uint8Array, ciphertext: Uint8Array): void
+    function enc_ecb_block(cipher: number, key: string | Uint8Array, plaintext: string | Uint8Array): Uint8Array
+    function enc_ecb_block(cipher: number, key: string | Uint8Array, plaintext: string | Uint8Array, ciphertext: Uint8Array): number
+    function dec_ecb_block(cipher: number, key: string | Uint8Array, ciphertext: Uint8Array): Uint8Array
+    function dec_ecb_block(cipher: number, key: string | Uint8Array, ciphertext: Uint8Array, plaintext: Uint8Array): number
+    function ecb(cipher: number, key: string | Uint8Array): ECB
+    function enc_ecb(state: Pointer, blocksize: number, plaintext: string | Uint8Array): Uint8Array
+    function enc_ecb(state: Pointer, blocksize: number, plaintext: string | Uint8Array, ciphertext: Uint8Array): number
+    function dec_ecb(state: Pointer, blocksize: number, ciphertext: Uint8Array): Uint8Array
+    function dec_ecb(state: Pointer, blocksize: number, ciphertext: Uint8Array, plaintext: Uint8Array): number
 }
+/**
+ * Implemented AES encryption and decryption algorithm
+ * 
+ * @remarks
+ * Either key length 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256
+ */
 export class AES {
-    static ecbEncrypt(key: string | Uint8Array, plaintext: string | Uint8Array): Uint8Array {
-        return deps.ecb_enc_block(deps.AES, key, plaintext)
+    /**
+     * returns the cipher's block size.
+     */
+    readonly blocksize = 16
+    /**
+     * Encrypt plaintext using ECB mode
+     * @param key AES key
+     * @param plaintext data to be encrypted
+     * @returns encrypted data
+     */
+    static encryptECB(key: string | Uint8Array, plaintext: string | Uint8Array): Uint8Array {
+        return deps.enc_ecb_block(deps.AES, key, plaintext)
     }
-    static ecbEncryptTo(key: string | Uint8Array, ciphertext: Uint8Array, plaintext: string | Uint8Array): number {
-        return deps.ecb_enc_block(deps.AES, key, plaintext, ciphertext)
+    /**
+     * Encrypt plaintext using ECB mode
+     * @param key AES key
+     * @param ciphertext encrypted data
+     * @param plaintext data to be encrypted
+     * @returns The length in bytes of the output ciphertext
+     */
+    static encryptECBTo(key: string | Uint8Array, ciphertext: Uint8Array, plaintext: string | Uint8Array): number {
+        return deps.enc_ecb_block(deps.AES, key, plaintext, ciphertext)
     }
-    static ecbDecrypt(key: string | Uint8Array, ciphertext: Uint8Array): Uint8Array {
-        const plaintext = new Uint8Array(16)
-        deps.ecb_dec_block(deps.AES, key, plaintext, ciphertext)
-        return ciphertext
+    /**
+     * Decrypt ciphertext using ECB mode
+     * @param key AES key
+     * @param ciphertext data to be decrypted
+     * @returns decrypted data
+     */
+    static decryptECB(key: string | Uint8Array, ciphertext: Uint8Array): Uint8Array {
+        return deps.dec_ecb_block(deps.AES, key, ciphertext)
     }
-    static ecbDecryptTo(key: string | Uint8Array, plaintext: Uint8Array, ciphertext: Uint8Array): void {
-        deps.ecb_dec_block(deps.AES, key, plaintext, ciphertext)
+    /**
+     * Decrypt ciphertext using ECB mode
+     * @param key AES key
+     * @param plaintext decrypted data
+     * @param ciphertext data to be decrypted
+     * @returns The length in bytes of the output plaintext
+     */
+    static decryptECBTo(key: string | Uint8Array, plaintext: Uint8Array, ciphertext: Uint8Array): number {
+        return deps.dec_ecb_block(deps.AES, key, ciphertext, plaintext)
     }
-
-    // constructor(key: string | Uint8Array) { }
-    // encrypt(plaintext?: string | Uint8Array): Uint8Array {
-    // }
-    // encryptTo(ciphertext: Uint8Array, plaintext?: string | Uint8Array) {
-    // }
+    /**
+     * 
+     * @param key AES key
+     * @returns ECB
+     */
+    static ecb(key: string | Uint8Array): ECB {
+        return new ECB(deps.ecb(deps.AES, key))
+    }
+}
+/**
+ * The most basic ECB encryption mode.
+ * @remarks
+ * In ECB mode, the plaintext and ciphertext must be integer multiples of blocksize.
+ * In this implementation, data that is not an integer multiple will be ignored, and the length of encryption and decryption will automatically take the smaller length of the input to avoid memory overflow.
+ */
+export class ECB {
+    constructor(readonly state: deps.ECB) { }
+    /**
+     * returns the cipher's block size.
+     */
+    get blocksize(): number {
+        return this.state.blocksize
+    }
+    /**
+     * Encrypt plaintext using ECB mode
+     * @param plaintext data to be encrypted
+     * @returns encrypted data
+     */
+    encrypt(plaintext: string | Uint8Array): Uint8Array {
+        const state = this.state
+        return deps.enc_ecb(state.p, state.blocksize, plaintext)
+    }
+    /**
+     * Encrypt plaintext using ECB mode
+     * @param ciphertext encrypted data
+     * @param plaintext data to be encrypted
+     * @returns The length in bytes of the output ciphertext
+     */
+    encryptTo(ciphertext: Uint8Array, plaintext: string | Uint8Array): number {
+        const state = this.state
+        return deps.enc_ecb(state.p, state.blocksize, plaintext, ciphertext)
+    }
+    /**
+     * Decrypt ciphertext using ECB mode
+     * @param ciphertext data to be decrypted
+     * @returns decrypted data
+     */
+    decrypt(ciphertext: Uint8Array): Uint8Array {
+        const state = this.state
+        return deps.dec_ecb(state.p, state.blocksize, ciphertext)
+    }
+    /**
+     * Decrypt ciphertext using ECB mode
+     * @param plaintext decrypted data
+     * @param ciphertext data to be decrypted
+     * @returns The length in bytes of the output plaintext
+     */
+    decryptTo(plaintext: Uint8Array, ciphertext: Uint8Array): number {
+        const state = this.state
+        return deps.dec_ecb(state.p, state.blocksize, ciphertext, plaintext)
+    }
 }
--- src/ejs/modules/crypto.c
+++ src/ejs/modules/crypto.c
@@ -1,7 +1,7 @@
 #include "modules_shared.h"
 #include <tomcrypt.h>
 #include "../js/crypto.h"
-static duk_ret_t ecb_enc_block(duk_context *ctx)
+static duk_ret_t ecb_block(duk_context *ctx, int dec)
 {
     int cipher = duk_require_uint(ctx, 0);
     if (cipher_is_valid(cipher) != CRYPT_OK)
@@ -14,14 +14,14 @@ static duk_ret_t ecb_enc_block(duk_context *ctx)
 
     duk_size_t key_len = 0;
     const uint8_t *key = EJS_REQUIRE_CONST_LSOURCE(ctx, 1, &key_len);
-    duk_size_t plaintext_len = 0;
-    const uint8_t *plaintext = EJS_REQUIRE_CONST_LSOURCE(ctx, 2, &plaintext_len);
-    plaintext_len -= plaintext_len % block_length;
+    duk_size_t input_len = 0;
+    const uint8_t *input = EJS_REQUIRE_CONST_LSOURCE(ctx, 2, &input_len);
+    input_len -= input_len % block_length;
 
     if (duk_is_undefined(ctx, 3))
     {
-        uint8_t *ciphertext = duk_push_fixed_buffer(ctx, plaintext_len);
-        if (plaintext_len > 0)
+        uint8_t *output = duk_push_fixed_buffer(ctx, input_len);
+        if (input_len > 0)
         {
             symmetric_ECB ecb;
             if (ecb_start(cipher, key, key_len, 0, &ecb) != CRYPT_OK)
@@ -29,25 +29,37 @@ static duk_ret_t ecb_enc_block(duk_context *ctx)
                 duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_start fail");
                 duk_throw(ctx);
             }
-            if (ecb_encrypt(plaintext, ciphertext, plaintext_len, &ecb) != CRYPT_OK)
+            if (dec)
             {
-                ecb_done(&ecb);
-                duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_encrypt fail");
-                duk_throw(ctx);
+                if (ecb_decrypt(input, output, input_len, &ecb) != CRYPT_OK)
+                {
+                    ecb_done(&ecb);
+                    duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_decrypt fail");
+                    duk_throw(ctx);
+                }
+            }
+            else
+            {
+                if (ecb_encrypt(input, output, input_len, &ecb) != CRYPT_OK)
+                {
+                    ecb_done(&ecb);
+                    duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_encrypt fail");
+                    duk_throw(ctx);
+                }
             }
             ecb_done(&ecb);
         }
         return 1;
     }
 
-    duk_size_t ciphertext_len = 0;
-    uint8_t *ciphertext = duk_require_buffer_data(ctx, 3, &ciphertext_len);
-    ciphertext_len -= ciphertext_len % block_length;
-    if (ciphertext_len < plaintext_len)
+    duk_size_t output_len = 0;
+    uint8_t *output = duk_require_buffer_data(ctx, 3, &output_len);
+    output_len -= output_len % block_length;
+    if (output_len < input_len)
     {
-        plaintext_len = ciphertext_len;
+        input_len = output_len;
     }
-    if (plaintext_len > 0)
+    if (input_len > 0)
     {
         symmetric_ECB ecb;
         if (ecb_start(cipher, key, key_len, 0, &ecb) != CRYPT_OK)
@@ -55,16 +67,28 @@ static duk_ret_t ecb_enc_block(duk_context *ctx)
             duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_start fail");
             duk_throw(ctx);
         }
-        if (ecb_encrypt(plaintext, ciphertext, plaintext_len, &ecb) != CRYPT_OK)
+        if (dec)
         {
-            ecb_done(&ecb);
-            duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_encrypt fail");
-            duk_throw(ctx);
+            if (ecb_decrypt(input, output, input_len, &ecb) != CRYPT_OK)
+            {
+                ecb_done(&ecb);
+                duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_decrypt fail");
+                duk_throw(ctx);
+            }
+        }
+        else
+        {
+            if (ecb_encrypt(input, output, input_len, &ecb) != CRYPT_OK)
+            {
+                ecb_done(&ecb);
+                duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_encrypt fail");
+                duk_throw(ctx);
+            }
         }
         ecb_done(&ecb);
 
         duk_pop_3(ctx);
-        duk_push_number(ctx, plaintext_len);
+        duk_push_number(ctx, input_len);
     }
     else
     {
@@ -72,6 +96,132 @@ static duk_ret_t ecb_enc_block(duk_context *ctx)
         duk_push_number(ctx, 0);
     }
     return 1;
+}
+static duk_ret_t enc_ecb_block(duk_context *ctx)
+{
+    return ecb_block(ctx, 0);
+}
+static duk_ret_t dec_ecb_block(duk_context *ctx)
+{
+    return ecb_block(ctx, 1);
+}
+static duk_ret_t ecb_finalizer(duk_context *ctx)
+{
+    duk_get_prop_lstring(ctx, -1, "p", 1);
+    uint8_t *p = duk_get_pointer_default(ctx, -1, 0);
+    if (p)
+    {
+        if (p[sizeof(symmetric_ECB)])
+        {
+            p[sizeof(symmetric_ECB)] = 0;
+            ecb_done((symmetric_ECB *)p);
+        }
+        free(p);
+    }
+    return 0;
+}
+static duk_ret_t ecb(duk_context *ctx)
+{
+    int cipher = duk_require_uint(ctx, 0);
+    if (cipher_is_valid(cipher) != CRYPT_OK)
+    {
+        duk_push_error_object(ctx, DUK_ERR_ERROR, "cipher invalid");
+        duk_throw(ctx);
+    }
+    duk_size_t block_length = cipher_descriptor[cipher].block_length;
+
+    duk_size_t key_len = 0;
+    const uint8_t *key = EJS_REQUIRE_CONST_LSOURCE(ctx, 1, &key_len);
+
+    uint8_t *p = ejs_push_finalizer_object(ctx, sizeof(symmetric_ECB) + 1, ecb_finalizer);
+    if (ecb_start(cipher, key, key_len, 0, (symmetric_ECB *)p) != CRYPT_OK)
+    {
+        duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_start fail");
+        duk_throw(ctx);
+    }
+    p[sizeof(symmetric_ECB)] = 1;
+
+    duk_push_uint(ctx, block_length);
+    duk_put_prop_lstring(ctx, -2, "blocksize", 9);
+    return 1;
+}
+static duk_ret_t ecb_stream(duk_context *ctx, int dec)
+{
+    symmetric_ECB *state = duk_require_pointer(ctx, 0);
+    duk_size_t block_length = duk_require_uint(ctx, 1);
+
+    duk_size_t input_len = 0;
+    const uint8_t *input = EJS_REQUIRE_CONST_LSOURCE(ctx, 2, &input_len);
+    input_len -= input_len % block_length;
+
+    if (duk_is_undefined(ctx, 3))
+    {
+        uint8_t *output = duk_push_fixed_buffer(ctx, input_len);
+        if (input_len > 0)
+        {
+            if (dec)
+            {
+                if (ecb_decrypt(input, output, input_len, state) != CRYPT_OK)
+                {
+                    duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_decrypt fail");
+                    duk_throw(ctx);
+                }
+            }
+            else
+            {
+                if (ecb_encrypt(input, output, input_len, state) != CRYPT_OK)
+                {
+                    duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_encrypt fail");
+                    duk_throw(ctx);
+                }
+            }
+        }
+        return 1;
+    }
+
+    duk_size_t output_len = 0;
+    uint8_t *output = duk_require_buffer_data(ctx, 3, &output_len);
+    output_len -= output_len % block_length;
+    if (output_len < input_len)
+    {
+        input_len = output_len;
+    }
+    if (input_len > 0)
+    {
+        if (dec)
+        {
+            if (ecb_decrypt(input, output, input_len, state) != CRYPT_OK)
+            {
+                duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_decrypt fail");
+                duk_throw(ctx);
+            }
+        }
+        else
+        {
+            if (ecb_encrypt(input, output, input_len, state) != CRYPT_OK)
+            {
+                duk_push_error_object(ctx, DUK_ERR_ERROR, "ecb_encrypt fail");
+                duk_throw(ctx);
+            }
+        }
+
+        duk_pop_3(ctx);
+        duk_push_number(ctx, input_len);
+    }
+    else
+    {
+        duk_pop_3(ctx);
+        duk_push_number(ctx, 0);
+    }
+    return 1;
+}
+static duk_ret_t enc_ecb(duk_context *ctx)
+{
+    return ecb_stream(ctx, 0);
+}
+static duk_ret_t dec_ecb(duk_context *ctx)
+{
+    return ecb_stream(ctx, 1);
 }
 EJS_SHARED_MODULE__DECLARE(crypto)
 {
@@ -91,8 +241,16 @@ EJS_SHARED_MODULE__DECLARE(crypto)
         duk_push_number(ctx, register_cipher(&aes_desc));
         duk_put_prop_lstring(ctx, -2, "AES", 3);
 
-        duk_push_c_lightfunc(ctx, ecb_enc_block, 4, 4, 0);
-        duk_put_prop_lstring(ctx, -2, "ecb_enc_block", 13);
+        duk_push_c_lightfunc(ctx, enc_ecb_block, 4, 4, 0);
+        duk_put_prop_lstring(ctx, -2, "enc_ecb_block", 13);
+        duk_push_c_lightfunc(ctx, dec_ecb_block, 4, 4, 0);
+        duk_put_prop_lstring(ctx, -2, "dec_ecb_block", 13);
+        duk_push_c_lightfunc(ctx, ecb, 2, 2, 0);
+        duk_put_prop_lstring(ctx, -2, "ecb", 3);
+        duk_push_c_lightfunc(ctx, enc_ecb, 4, 4, 0);
+        duk_put_prop_lstring(ctx, -2, "enc_ecb", 7);
+        duk_push_c_lightfunc(ctx, dec_ecb, 4, 4, 0);
+        duk_put_prop_lstring(ctx, -2, "dec_ecb", 7);
     }
 
     /*
